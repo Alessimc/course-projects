647201b1b1d8f8f76ca946501e6859ce7c54bb97
Alessimc <alessio.canclini@gmail.com>
2024-08-23 12:20:16 +0200

    Fixing all functions such that tests passs

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,30 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    N_t = len(u)
+    discrete_derivative = np.zeros(len(u))
+
+    # Handle edge cases with forward and backward differences
+    discrete_derivative[0] = (u[1] - u[0]) / dt
+
+    discrete_derivative[-1] = (u[-1] - u[-2]) / dt
+
+    # Centered difference
+    for n in range(1, N_t-1):
+        discrete_derivative[n] = (u[n+1] - u[n-1]) / (2 * dt)
+    return discrete_derivative
 
 def differentiate_vector(u, dt):
-    pass
+    d = np.zeros(len(u))
+
+    # Handle edge cases with forward and backward differences
+    d[0] = (u[1] - u[0]) / dt
+
+    d[-1] = (u[-1] - u[-2]) / dt
+    
+    # Centered difference
+    d[1:-1] = (u[2:] -u[0:-2])/(2*dt)
+    return d
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
--- projects/mesh_function.py
+++ projects/mesh_function.py
@@ -2,13 +2,42 @@ import numpy as np
 
 
 def mesh_function(f, t):
-    pass
+    mesh_point_values = np.zeros(len(t))
+    for i in range(len(t)):
+        mesh_point_values[i] = f(t[i])
+
+    return mesh_point_values    
 
 def func(t):
-    pass
+    return np.where((0 <= t) & (t <= 3), np.exp(-t),
+                    np.where((3 < t) & (t <= 4), np.exp(-3*t), 0))
 
 def test_mesh_function():
     t = np.array([1, 2, 3, 4])
     f = np.array([np.exp(-1), np.exp(-2), np.exp(-3), np.exp(-12)])
     fun = mesh_function(func, t)
     assert np.allclose(fun, f)
+
+# plotting
+import plotly.graph_objs as go
+N = 40
+n = np.arange(0, N+1)
+dt = 0.1
+t = n*dt
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=t, y=func(t), mode='lines', name='f(t) = sin(t)'))
+
+fig.update_layout(
+    title=r'$\text{Plot of }f(t) = \begin{cases} '
+          r'\exp(-t) & \text{if } 0 \leq t \leq 3, \\'
+          r'\exp(-3t) & \text{if } 3 < t \leq 4, \\'
+          r'\end{cases}$',
+    xaxis_title='t',
+    yaxis_title='f(t)',
+    template='plotly_dark',
+    font=dict(size=16)
+)
+
+# fig.show()
